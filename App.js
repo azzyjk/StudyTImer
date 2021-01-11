7da866035a56519484283020612aa89217e7de8f
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-01-11 20:58:51 +0900

    Face detecter using expo

---
+++ App.js
@@ -0,0 +1,90 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as FaceDetector from "expo-face-detector";
+import { Camera } from "expo-camera";
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  // const [handleFacesDetected, setHandleFacesDetected] = useState([]);
+  const [faces, setFaces] = useState([]);
+
+  const handleFacesDetected = ({ faces }) => {
+    if (faces.length > 0) {
+      // setFaces = { faces };
+      console.log(`$faces[0].faceID`);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+  console.log("hello");
+  if (hasPermission == null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text> No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.Constants.Mode.fast,
+          detectLandmarks: FaceDetector.Constants.Landmarks.any,
+          runClassifications: FaceDetector.Constants.Classifications.none,
+          minDetetectionInterval: 100,
+          tracking: true,
+        }}
+      >
+        {/* <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.text}>Flip</Text>
+          </TouchableOpacity>
+        </View> */}
+      </Camera>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  camera: {
+    flex: 0,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
