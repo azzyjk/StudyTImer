5397d705ae501d593e45fc5b702fbe5515411379
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-01-15 23:55:53 +0900

    Time increase while face detected

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.front);
   const [faces, setFaces] = useState([]);
   const [time, setTime] = useState({ hour: 0, min: 0, sec: 0 });
+  const [clock, setClock] = useState({ hour: 0, min: 0, sec: 0 });
 
   useEffect(() => {
     (async () => {
@@ -17,48 +18,39 @@ export default function App() {
     })();
   }, []);
 
-  // useEffect(() => {
-  // setInterval(() => {
-  //   console.log(`faces : ${faces.length}`);
-  //   if (faces.length != 0) {
-  //     console.log("interval 1000");
-  //     // setTime(sec + 1);
-  //   }
-  // }, 1000);
-  // if (faces.length > 0) {
-  //   console.log("face not 0");
-  // }
-  // }, []);
-
   useEffect(() => {
     const _timeIncrease = setInterval(() => {
-      var hour = parseInt(time.hour, 10);
-      var min = parseInt(time.min, 10);
-      var sec = parseInt(time.sec, 10);
-      setTime({ hour: hour, min: min, sec: sec + 1 });
+      var getTime = {
+        hour: parseInt(time.hour, 10),
+        min: parseInt(time.min, 10),
+        sec: parseInt(time.sec, 10),
+      };
+      var getClock = {
+        hour: parseInt(clock.hour, 10),
+        min: parseInt(clock.min, 10),
+        sec: parseInt(clock.sec, 10),
+      };
+      setClock({
+        hour: getClock.hour,
+        min: getClock.min,
+        sec: getClock.sec + 1,
+      });
+      console.log(faces.length);
+      if (faces.length != 0) {
+        setTime({ hour: getTime.hour, min: getTime.min, sec: getTime.sec + 1 });
+      }
       clearInterval(_timeIncrease);
     }, 1000);
-  }, [time.sec]);
-
-  const _timeIncrease = () => {};
-
-  // var hour = parseInt(time.hour, 10);
-  // var min = parseInt(time.min, 10);
-  // var sec = parseInt(time.sec, 10);-
-  // setTime({ hour: hour, min: min, sec: sec + 1 });
+  }, [clock]);
 
   const _handleFacesDetected = ({ faces }) => {
-    // console.log(faces);
     if (faces.length > 0) {
       setFaces(faces);
-      console.log(`${faces[0].faceID}`);
-      // console.log(`hour : ${time.hour} min : ${time.min} sec : ${time.sec}`);
-      // console.log(faces[0]);
-      // `smile : ${faces[0].smilingProbability}, left eye open : ${faces[0].leftEyeOpenProbability}`
+    } else {
+      setFaces([]);
     }
   };
 
-  // console.log("hello");
   if (hasPermission == null) {
     return <View />;
   }
@@ -67,36 +59,31 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        onFacesDetected={_handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.Constants.Mode.fast,
+          detectLandmarks: FaceDetector.Constants.Landmarks.all,
+          runClassifications: FaceDetector.Constants.Classifications.all,
+          minDetetectionInterval: 1000,
+          tracking: true,
+        }}
+      ></Camera>
       <Text style={styles.timerText}>
         {time.hour}시 {time.min}분 {time.sec}초
       </Text>
     </View>
   );
-  // return (
-  //   <View style={styles.container}>
-  //     <Camera
-  //       style={styles.camera}
-  //       type={type}
-  //       onFacesDetected={_handleFacesDetected}
-  //       faceDetectorSettings={{
-  //         mode: FaceDetector.Constants.Mode.fast,
-  //         detectLandmarks: FaceDetector.Constants.Landmarks.all,
-  //         runClassifications: FaceDetector.Constants.Classifications.all,
-  //         minDetetectionInterval: 1000,
-  //         tracking: true,
-  //       }}
-  //     ></Camera>
-  //   </View>
-  // );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   camera: {
-    flex: 1,
+    flex: 0.5,
   },
   buttonContainer: {
     flex: 1,
